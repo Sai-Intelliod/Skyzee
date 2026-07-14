725dea1102898600e97091cf50b34a277371f454
Sai-Intelliod <sai.kumar@intelliod.com>
2026-07-14 15:10:18 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 6001,
+    port: 6000,
     host: '0.0.0.0',
     allowedHosts: [
       'skyzee.intelliod.com'
